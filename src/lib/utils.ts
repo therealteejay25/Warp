83d3e6e217669f0b73e1b412eade793447644435
therealteejay25 <therealteejay25@gmail.com>
2025-09-24 19:04:34 +0100

    Refactor cn() function to enforce string input type for better type safety.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,6 +2,6 @@ import { clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 // cn() combines clsx + twMerge for smarter merging
-export function cn(...inputs: any[]) {
+export function cn(...inputs: string[]) {
   return twMerge(clsx(inputs))
 }
